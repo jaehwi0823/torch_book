cb9fd092186b239260ab452c730c12e2383f6316
jaehwi <iamhwii@gmail.com>
2021-08-27 01:04:26 +0900

    keep going on ch1

--- ch01_01.py
+++ ch01_01.py
@@ -1,12 +1,17 @@
 import numpy as np
 import json
+import glob
+import os.path as osp
+import torch.utils.data as data
 from PIL import Image
 import matplotlib.pyplot as plt
 %matplotlib inline
 import torch
 import torchvision
 from torchvision import models, transforms
-
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
 # print(torch.__version__)
 # print(torchvision.__version__)
 
@@ -29,8 +34,9 @@ class BaseTransform():
 
 img_path = './data/goldenretriever-3724972_640.jpg'
 img = Image.open(img_path)
+# transforms.ToTensor()(img).shape
 plt.imshow(img)
-img.show()
+plt.show()
 
 # transform test
 tran = BaseTransform(resize=224,
@@ -38,8 +44,347 @@ tran = BaseTransform(resize=224,
                      std = (0.229, 0.224, 0.225))
 img_tran = tran(img)
 img_tran = torch.permute(img_tran,(1,2,0))
-img_tran = torch.clip(img_tran, 0, 1)
+# img_tran = torch.clip(img_tran, 0, 1)
 plt.imshow(img_tran)
 plt.show()
 
 # post-precessing
+c_index = json.load(open('./data/imagenet_class_index.json', 'r'))
+input_ = torch.permute(tran(img), (1,2,0)).unsqueeze_(0)
+input_ = tran(img).unsqueeze_(0)
+input_.shape
+out = net(input_)
+out
+
+
+
+
+
+class ImageTransform():
+    """
+    화상 전처리 클래스. 훈련시, 검증시의 동작이 다르다.
+    화상 크기를 리사이즈하고, 색상을 표준화한다.
+    훈련시에는 RandomResizedCrop과 RandomHorizontalFlip으로 데이터를 확장한다.
+
+    Attributes
+    ----------
+    resize : int
+        리사이즈 대상 화상의 크기.
+    mean : (R, G, B)
+        각 색상 채널의 평균값.
+    std : (R, G, B)
+        각 색상 채널의 표준 편차.
+    """
+
+    def __init__(self, resize, mean, std):
+        self.data_transform = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(
+                    resize, scale=(0.5, 1.0)),  # 데이터 확장
+                transforms.RandomHorizontalFlip(),  # 데이터 확장
+                transforms.ToTensor(),  # 텐서로 변환
+                transforms.Normalize(mean, std)  # 표준화
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(resize),  # リサイズ
+                transforms.CenterCrop(resize),  # 화상 중앙을 resize × resize로 자른다
+                transforms.ToTensor(),  # 텐서로 변환
+                transforms.Normalize(mean, std)  # 표준화
+            ])
+        }
+
+    def __call__(self, img, phase='train'):
+        """
+        Parameters
+        ----------
+        phase : 'train' or 'val'
+            전처리 모드를 지정.
+        """
+        return self.data_transform[phase](img)
+
+
+def make_datapath_list(phase="train"):
+    """
+    데이터의 경로를 저장한 리스트를 작성한다.
+
+    Parameters
+    ----------
+    phase : 'train' or 'val'
+        훈련 데이터 또는 검증 데이터를 지정
+
+    Returns
+    -------
+    path_list : list
+        데이터 경로를 저장한 리스트
+    """
+
+    rootpath = "./data/hymenoptera_data/"
+    target_path = osp.join(rootpath+phase+'/**/*.jpg')
+    print(target_path)
+
+    path_list = []  # 여기에 저장한다
+
+    # glob을 이용하여 하위 디렉토리의 파일 경로를 가져온다
+    for path in glob.glob(target_path):
+        path_list.append(path)
+
+    return path_list
+
+
+class HymenopteraDataset(data.Dataset):
+    """
+    개미와 벌 화상의 Dataset 클래스. PyTorch의 Dataset 클래스를 상속한다.
+
+    Attributes
+    ----------
+    file_list : 리스트
+        화상 경로를 저장한 리스트
+    transform : object
+        전처리 클래스의 인스턴스
+    phase : 'train' or 'test'
+        학습인지 훈련인지를 설정한다.
+    """
+
+    def __init__(self, file_list, transform=None, phase='train'):
+        self.file_list = file_list  # 파일 경로 리스트
+        self.transform = transform  # 전처리 클래스의 인스턴스
+        self.phase = phase  # train or val 지정
+
+    def __len__(self):
+        '''화상 개수를 반환'''
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        '''
+        전처리한 화상의 Tensor 형식의 데이터와 라벨을 취득
+        '''
+
+        # index번째의 화상을 로드
+        img_path = self.file_list[index]
+        img = Image.open(img_path)  # [높이][폭][색RGB]
+
+        # 화상의 전처리를 실시
+        img_transformed = self.transform(
+            img, self.phase)  # torch.Size([3, 224, 224])
+
+        # 화상 라벨을 파일 이름에서 추출
+        if self.phase == "train":
+            label = img_path[30:34]
+        elif self.phase == "val":
+            label = img_path[28:32]
+
+        # 라벨을 숫자로 변경
+        if label == "ants":
+            label = 0
+        elif label == "bees":
+            label = 1
+
+        return img_transformed, label
+
+
+# 실행
+train_list = make_datapath_list(phase="train")
+val_list = make_datapath_list(phase="val")
+
+
+
+# 실행
+size=224
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
+train_dataset = HymenopteraDataset(
+    file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
+
+val_dataset = HymenopteraDataset(
+    file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
+
+
+# 미니 배치 크기 지정
+batch_size = 32
+
+# DataLoader 작성
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=batch_size, shuffle=False)
+
+# 사전형 변수에 정리
+dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+
+# 동작 확인
+batch_iterator = iter(dataloaders_dict["train"])  # 반복자(iterator)로 변환
+inputs, labels = next(
+    batch_iterator)  # 첫번째 요소를 추출
+print(inputs.size())
+print(labels)
+
+
+
+# 학습된 VGG-16 모델을 로드
+# VGG-16 모델의 인스턴스 생성
+use_pretrained = True  # 학습된 파라미터를 사용
+net = models.vgg16(pretrained=use_pretrained)
+
+# VGG16의 마지막 출력층의 출력 유닛을 개미와 벌의 2개로 바꾼다
+net.classifier[6] = nn.Linear(in_features=4096, out_features=2)
+
+# 훈련 모드로 설정
+net.train()
+
+# loss function
+criterion = nn.CrossEntropyLoss()
+
+
+# 전이학습에서 학습시킬 파라미터를 params_to_update 변수에 저장한다
+params_to_update = []
+
+# 학습시킬 파라미터 이름
+update_param_names = ["classifier.6.weight", "classifier.6.bias"]
+
+# 학습시킬 파라미터 이외에는 경사를 계산하지 않고, 변하지 않게 설정
+for name, param in net.named_parameters():
+    if name in update_param_names:
+        param.requires_grad = True
+        params_to_update.append(param)
+        print(name)
+    else:
+        param.requires_grad = False
+
+# params_to_update의 내용을 확인
+print("-----------")
+print(params_to_update)
+
+# 최적화 기법 설정
+optimizer = optim.SGD(params=params_to_update, lr=0.001, momentum=0.9)
+
+
+# 모델을 학습시키는 함수를 작성
+def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs):
+
+    # epoch 루프
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-------------')
+
+        # epoch별 학습 및 검증 루프
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                net.train()  # 모델을 훈련 모드로
+            else:
+                net.eval()   # 모델을 검증 모드로
+
+            epoch_loss = 0.0  # epoch 손실의 합
+            epoch_corrects = 0  # epoch 정답의 수
+
+            # 미 학습시 검증 성능을 확인하기 위해 epoch=0의 훈련 생략
+            if (epoch == 0) and (phase == 'train'):
+                continue
+
+            # 데이터 로더로 미니 배치를 꺼내는 루프
+            for inputs, labels in tqdm(dataloaders_dict[phase]):
+
+                # optimizer를 초기화
+                optimizer.zero_grad()
+
+                # 순전파(forward) 계산
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)  # 손실을 계산
+                    _, preds = torch.max(outputs, 1)  # 라벨을 예측
+                    
+  
+                    # 훈련시에는 오차 역전파
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                    # 반복 결과 계산
+                    # loss 합계 갱신
+                    epoch_loss += loss.item() * inputs.size(0)  
+                    # 정답 수의 합계 갱신
+                    epoch_corrects += torch.sum(preds == labels.data)
+
+            # epoch 당 loss와 정답률 표시
+            epoch_loss = epoch_loss / len(dataloaders_dict[phase].dataset)
+            epoch_acc = epoch_corrects.double(
+            ) / len(dataloaders_dict[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+# 학습 및 검증 실시
+num_epochs=2
+train_model(net, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
+
+
+
+
+# 튜토리얼과 비교
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # 각 에폭(epoch)은 학습 단계와 검증 단계를 갖습니다.
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # 모델을 학습 모드로 설정
+            else:
+                model.eval()   # 모델을 평가 모드로 설정
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # 데이터를 반복
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # 매개변수 경사도를 0으로 설정
+                optimizer.zero_grad()
+
+                # 순전파
+                # 학습 시에만 연산 기록을 추적
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # 학습 단계인 경우 역전파 + 최적화
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # 통계
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # 모델을 깊은 복사(deep copy)함
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # 가장 나은 모델 가중치를 불러옴
+    model.load_state_dict(best_model_wts)
+    return model
